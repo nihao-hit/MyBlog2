24df29bb59fdabcd8f6df6606a0f15fcf7cdf680
chenxin <nihao_cx@163.com>
2018-04-30 14:29:39 +0800

    第三次提交，完成欢迎界面

--- app/src/main/java/com/example/myblog/WelcomeActivity.java
+++ app/src/main/java/com/example/myblog/WelcomeActivity.java
@@ -1,13 +1,25 @@
 package com.example.myblog;
 
+import android.content.Intent;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class WelcomeActivity extends AppCompatActivity {
-
+    public Handler handler = new Handler(){
+        @Override
+        public void handleMessage(Message msg){
+            Intent intent = new Intent(WelcomeActivity.this,MainActivity.class);
+            startActivity(intent);
+            finish();
+            super.handleMessage(msg);
+        }
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+        handler.sendEmptyMessageDelayed(0,3000);
     }
 }
